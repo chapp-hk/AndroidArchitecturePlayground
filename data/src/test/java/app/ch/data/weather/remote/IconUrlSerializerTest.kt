52c56a4582756a8ae61b440127ca9cfdc528ee78
chapp <chapp.hkg@gmail.com>
2021-03-05 02:06:18 +0800

    refactor: simplify test code

--- data/src/test/java/app/ch/data/weather/remote/IconUrlSerializerTest.kt
+++ data/src/test/java/app/ch/data/weather/remote/IconUrlSerializerTest.kt
@@ -1,12 +1,10 @@
 package app.ch.data.weather.remote
 
 import app.ch.data.BuildConfig
-import io.mockk.MockKAnnotations
 import io.mockk.every
-import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.encoding.Decoder
-import org.junit.Before
 import org.junit.Test
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
@@ -14,19 +12,11 @@ import strikt.assertions.isEqualTo
 @ExperimentalSerializationApi
 class IconUrlSerializerTest {
 
-    @MockK
-    private lateinit var decoder: Decoder
-
-    @Before
-    fun setUp() {
-        MockKAnnotations.init(this)
-    }
-
     @Test
     fun deserialize() {
-        every {
-            decoder.decodeString()
-        } returns "iconId"
+        val decoder = mockk<Decoder> {
+            every { decodeString() } returns "iconId"
+        }
 
         expectThat(IconUrlSerializer.deserialize(decoder))
             .isEqualTo(BuildConfig.ICON_URL.replace("{place-holder}", "iconId"))
