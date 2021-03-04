cba977104346c6cba4e3348bc30ce7bf6543285e
chapp <chapp.hkg@gmail.com>
2021-03-05 00:41:40 +0800

    build: add fresco dependency

--- app/src/main/java/app/ch/weatherapp/App.kt
+++ app/src/main/java/app/ch/weatherapp/App.kt
@@ -1,6 +1,7 @@
 package app.ch.weatherapp
 
 import android.app.Application
+import com.facebook.drawee.backends.pipeline.Fresco
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -9,6 +10,8 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        Fresco.initialize(this)
 
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
