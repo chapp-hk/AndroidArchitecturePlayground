a828371914c4e615cc87560755afb9bf08ee9032
chapp <chapp.hkg@gmail.com>
2021-03-12 22:36:06 +0800

    fix: change location providers to mocks

---
+++ mobile-service/src/androidTest/java/app/ch/mobile/service/gms/location/TestLocationModule.kt
@@ -0,0 +1,51 @@
+package app.ch.mobile.service.gms.location
+
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.SettingsClient
+import com.google.android.gms.tasks.CancellationToken
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import io.mockk.mockk
+import javax.inject.Singleton
+
+@Module
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [LocationModule::class]
+)
+class TestLocationModule {
+
+    @Provides
+    @Singleton
+    internal fun providesFusedLocationProviderClient(): FusedLocationProviderClient {
+        return mockk(relaxed = true, relaxUnitFun = true)
+    }
+
+    @Provides
+    @Singleton
+    internal fun providesCancellationToken(): CancellationToken {
+        return mockk(relaxed = true, relaxUnitFun = true)
+    }
+
+    @Provides
+    @Singleton
+    internal fun providesLocationRequest(): LocationRequest {
+        return mockk(relaxed = true, relaxUnitFun = true)
+    }
+
+    @Provides
+    @Singleton
+    internal fun providesLocationSettingsRequest(locationRequest: LocationRequest): LocationSettingsRequest {
+        return mockk(relaxed = true, relaxUnitFun = true)
+    }
+
+    @Provides
+    @Singleton
+    internal fun providesLocationSettingsClient(): SettingsClient {
+        return mockk(relaxed = true, relaxUnitFun = true)
+    }
+}
